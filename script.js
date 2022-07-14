acb6bd75ef2f40c6be8d35c2de0a6e2fba0fdd54
Chris9694 <69814180+tv4Interactivo@users.noreply.github.com>
2022-07-14 16:48:45 -0500

    version 1.0.5

--- script.js
+++ script.js
@@ -14,14 +14,58 @@ const onProgress = (event) => {
   };
   document.querySelector('model-viewer').addEventListener('progress', onProgress);
 
+//Show/Hide dropdown menu
+function toggleDisplay(){
+  const toggle = document.getElementById("dropdown-content")
+    if(toggle.style.display === "none"){
+      toggle.style.display = "block"
+    } else {
+      toggle.style.display = "none"
+}
+}
+
+document.addEventListener('mouseup', function(e) {
+  var container = document.getElementById("dropdown-content");
+  if (!container.contains(e.target)) {
+      container.style.display = 'none';
+  }
+});
+
+//copy url to clipboard
+function copyURL(){
+  const btnHome = document.getElementsByClassName("share")
+  navigator.clipboard.writeText(window.location.href) 
+  
+  const popup = document.getElementById("alert")
+  popup.style.display = "block"
+  
+}
+
+function closeAlert(){
+  const closeBtn = document.getElementById("alert")
+  closeBtn.style.display = "none"
+}
+
+document.addEventListener('mouseup', function(e) {
+  var container = document.getElementById("alert");
+  if (!container.contains(e.target)) {
+      container.style.display = 'none';
+  }
+});
 
 
+//change language
+function changeLang(){
+  const esp = document.getElementById("text-esp")
+  const eng = document.getElementById("text-eng")
+  if(esp.style.display === "block"){
+    esp.style.display = "none"
+    eng.style.display = "block"
+  } else{
+    esp.style.display = "block"
+    eng.style.display = "none"
+  }
+  
+}
 
-  // Look for .hamburger
-  var hamburger = document.querySelector(".hamburger");
-  // On click
-  hamburger.addEventListener("click", function() {
-    // Toggle class "is-active"
-    hamburger.classList.toggle("is-active");
-    // Do something else, like open/close menu
-  });
+
