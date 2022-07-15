cea734130767cd052e5621603f2e552b6437291d
Chris9694 <69814180+tv4Interactivo@users.noreply.github.com>
2022-07-15 13:50:51 -0500

    version 1.0.6

--- script.js
+++ script.js
@@ -1,71 +1,62 @@
 // Handles loading the events for <model-viewer>'s slotted progress bar
 const onProgress = (event) => {
-    const progressBar = event.target.querySelector('.progress-bar');
-    const updatingBar = event.target.querySelector('.update-bar');
-    updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
-    if (event.detail.totalProgress === 1) {
-      progressBar.classList.add('hide');
-    } else {
-      progressBar.classList.remove('hide');
-      if (event.detail.totalProgress === 0) {
-        event.target.querySelector('.center-pre-prompt').classList.add('hide');
-      }
+  const progressBar = event.target.querySelector(".progress-bar");
+  const updatingBar = event.target.querySelector(".update-bar");
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add("hide");
+  } else {
+    progressBar.classList.remove("hide");
+    if (event.detail.totalProgress === 0) {
+      event.target.querySelector(".center-pre-prompt").classList.add("hide");
     }
-  };
-  document.querySelector('model-viewer').addEventListener('progress', onProgress);
+  }
+};
+document.querySelector("model-viewer").addEventListener("progress", onProgress);
+
+const toggle = document.getElementById("dropdown-content");
 
 //Show/Hide dropdown menu
-function toggleDisplay(){
-  const toggle = document.getElementById("dropdown-content")
-    if(toggle.style.display === "none"){
-      toggle.style.display = "block"
-    } else {
-      toggle.style.display = "none"
-}
-}
-
-document.addEventListener('mouseup', function(e) {
-  var container = document.getElementById("dropdown-content");
-  if (!container.contains(e.target)) {
-      container.style.display = 'none';
+function toggleDisplay() {
+  if (toggle.style.display === "none") {
+    toggle.style.display = "block";
+  } else {
+    toggle.style.display = "none";
   }
-});
+}
 
 //copy url to clipboard
-function copyURL(){
-  const btnHome = document.getElementsByClassName("share")
-  navigator.clipboard.writeText(window.location.href) 
-  
-  const popup = document.getElementById("alert")
-  popup.style.display = "block"
-  
+function copyURL() {
+  const btnHome = document.getElementsByClassName("share");
+  navigator.clipboard.writeText(window.location.href);
+
+  const popup = document.getElementById("alert");
+  popup.style.display = "block";
+  toggle.style.display = "none";
 }
 
-function closeAlert(){
-  const closeBtn = document.getElementById("alert")
-  closeBtn.style.display = "none"
+function closeAlert() {
+  const closeBtn = document.getElementById("alert");
+  closeBtn.style.display = "none";
 }
 
-document.addEventListener('mouseup', function(e) {
+document.addEventListener("mouseup", function (e) {
   var container = document.getElementById("alert");
   if (!container.contains(e.target)) {
-      container.style.display = 'none';
+    container.style.display = "none";
   }
 });
 
-
 //change language
-function changeLang(){
-  const esp = document.getElementById("text-esp")
-  const eng = document.getElementById("text-eng")
-  if(esp.style.display === "block"){
-    esp.style.display = "none"
-    eng.style.display = "block"
-  } else{
-    esp.style.display = "block"
-    eng.style.display = "none"
+function changeLang() {
+  const esp = document.getElementById("text-esp");
+  const eng = document.getElementById("text-eng");
+  if (esp.style.display === "block") {
+    esp.style.display = "none";
+    eng.style.display = "block";
+  } else {
+    esp.style.display = "block";
+    eng.style.display = "none";
   }
-  
+  toggle.style.display = "none";
 }
-
-
